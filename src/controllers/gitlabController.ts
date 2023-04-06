f871754530d3575bc1b494b8e073d87ae2075c03
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-04-06 16:42:30 +0200

    feat: finalize controller gitlab

--- src/controllers/gitlabController.ts
+++ src/controllers/gitlabController.ts
@@ -1,53 +1,86 @@
 import { Request, Response } from 'express';
-import { Controller } from '@/server/Controllers';
-import { Slack } from '@/services/slack';
-import { validateMergeRequest } from '@/util/validateMergeRequest';
-import { logger } from '@/server/Logger';
 
+import { Controller } from '@/server/Controllers';
 import type { GitlabEvent } from '@/types/index';
+import { Slack } from '@/services/slack';
+import { createSlackMessage } from '@/util/slack/getMergeMessage';
+import { getTimeStampMessage } from '@/util/slack/getTimeStamp';
+import { logger } from '@/server/Logger';
+import { validateMergeRequest } from '@/util/validateMergeRequest';
 
 const controller = new Controller('gitlabController');
+
+const slack = new Slack();
 
 controller.post('/', [], async (req: Request, res: Response) => {
   const event = req.header('X-Gitlab-Event');
   const payload = req.body;
-
-  if (event === 'Merge Request Hook') {
-    try {
+  try {
+    if (event === 'Merge Request Hook') {
       await handleMergeRequestEvent(payload);
-    } catch (error) {
-      console.error('Error handling merge request event:', error);
     }
+    res.sendStatus(200);
+  } catch (error) {
+    logger.error('Error handling GitLab event:', error);
+    res.status(500).send('Error handling GitLab event');
   }
-
-  res.sendStatus(200);
 });
 
 async function handleMergeRequestEvent(payload: GitlabEvent) {
-  const user = payload.user.name;
-  const { id, name } = payload.project;
-  const { state, action, iid, url, source_branch, target_branch, work_in_progress } =
-    payload.object_attributes;
+  const {
+    user: { name: user },
+    project: { id, name },
+    object_attributes: {
+      title,
+      state,
+      action,
+      iid,
+      url,
+      source_branch,
+      target_branch,
+      work_in_progress,
+      description,
+    },
+  } = payload;
 
   if (!id || !iid) throw new Error('Invalid project ID or merge request ID');
 
   if (action === 'open' && state === 'opened' && !work_in_progress) {
     try {
-      const slack = new Slack();
-      const text = `Merge Request: for ${name} created by ${user} - ${source_branch} -> ${target_branch} - ${url}`;
-
-      const resultSlack = await slack.sendMessage('pull-request-dev', text);
-      logger.info('Slack message sent', resultSlack);
+      const text = createSlackMessage({
+        id,
+        name,
+        title,
+        user,
+        description,
+        url,
+        source_branch,
+        target_branch,
+      });
+      await slack.sendMessage(text);
+      logger.info('Slack message sent');
     } catch (error) {
       logger.error('Error sending message to Slack:', error);
     }
-
     try {
-      const mergeRequest = await validateMergeRequest(id, iid);
-      logger.info('Merge request validated', mergeRequest);
+      await validateMergeRequest(id, iid);
     } catch (error) {
       logger.error('Error validating merge request:', error);
+    } finally {
+      finalizeMergeRequest(id);
     }
+  }
+}
+
+async function finalizeMergeRequest(id: number): Promise<void> {
+  try {
+    const timestamp = await getTimeStampMessage(id);
+    await slack.sendMessage(
+      { text: 'Hi, I added some feedback to your merge request :robot_face:' },
+      timestamp,
+    );
+  } catch (error) {
+    console.error(`Failed to finalize merge request with ID ${id}:`, error);
   }
 }
 
