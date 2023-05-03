419b9a0624713b2a22fa999a09149d1d99ab7fb7
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-05-03 13:32:05 +0200

    fix: removed unused func

--- src/helpers/truncateString.ts
+++
@@ -1,8 +0,0 @@
-export default function truncateString(string: string, maxChar: number) {
-  if (string && string.length > maxChar) {
-    const regex = new RegExp(`^(.{${maxChar}}[^\\s]*).*`);
-    return string.replace(regex, '$1') + '...';
-  } else {
-    return string;
-  }
-}
