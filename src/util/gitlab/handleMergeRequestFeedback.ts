8fc0d0fe15a4818daff801a140bba8d07be4c5a9
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-05-23 16:40:24 +0200

    perf: splitted up feedback process

--- src/util/gitlab/handleMergeRequestFeedback.ts
+++ src/util/gitlab/handleMergeRequestFeedback.ts
@@ -23,49 +23,68 @@ async function handleMergeRequestFeedback(
   sourceBranch: string,
 ): Promise<void> {
   const url = `projects/${projectId}/merge_requests/${mergeRequestId}/diffs`;
-  const commentManager = new CommentManager();
 
   try {
     const changes: GitLabChanges[] = await new GitLab('GET', url).connect();
 
-    const promises = changes.map(async (change: GitLabChanges) => {
-      try {
-        const { diff, new_path, deleted_file, old_path } = change;
-
-        if (!diff) return;
-
-        const language: string | false = await identifyFile(new_path);
-
-        if (deleted_file || !language) {
-          Logger.info(`Ignored: ${new_path}`);
-          return;
-        }
-
-        const lineNumber: number | undefined = await getLastChangedLine(
-          change,
-          sourceBranch,
-          projectId,
-        );
-
-        if (!lineNumber) return;
-
-        const framework = await identifyFramework(projectId);
-
-        const feedback: string | undefined = await getFeedback(change, language, framework);
-
-        if (!feedback) return;
-
-        commentManager.create(projectId, mergeRequestId, old_path, new_path, feedback, lineNumber);
-      } catch (error) {
-        Logger.error(`Error processing change ${change.new_path}: ${error}`);
-      }
-    });
+    const errors: Error[] = [];
+    const promises = changes.map((change) =>
+      processChange(change, mergeRequestId, sourceBranch, projectId).catch((error) =>
+        errors.push(error),
+      ),
+    );
 
     await Promise.all(promises);
+
+    if (errors.length > 0) {
+      Logger.error(`Some changes failed to process ${errors}`);
+      throw new Error('Some changes failed to process');
+    }
 
     Logger.info('Merge request validated');
   } catch (error) {
     Logger.error(`Error handling merge request feedback: ${error}`);
+  }
+}
+
+async function processChange(
+  change: GitLabChanges,
+  mergeRequestId: number,
+  sourceBranch: string,
+  projectId: number,
+): Promise<void> {
+  const commentManager = new CommentManager();
+
+  try {
+    const { diff, new_path, deleted_file, old_path } = change;
+
+    if (!diff) return;
+
+    const language: string | false = await identifyFile(new_path);
+
+    if (deleted_file || !language) {
+      Logger.info(`Ignored: ${new_path}`);
+      return;
+    }
+
+    const lineNumber: number | undefined = await getLastChangedLine(
+      change,
+      sourceBranch,
+      projectId,
+    );
+
+    if (!lineNumber) return;
+
+    const framework = await identifyFramework(projectId);
+
+    const feedback: string | undefined = await getFeedback(change, language, framework);
+
+    if (!feedback) return;
+
+    commentManager.create(projectId, mergeRequestId, old_path, new_path, feedback, lineNumber);
+  } catch (error) {
+    Logger.error(`Error processing change ${change.new_path}: ${error}`);
+    throw error;
   }
 }
 
