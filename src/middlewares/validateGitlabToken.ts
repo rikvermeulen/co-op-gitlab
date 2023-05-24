2a9002500e33c949ae9d3795168fc4a9c72c53b3
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-05-24 17:25:41 +0200

    fix: removed check

--- src/middlewares/validateGitlabToken.ts
+++ src/middlewares/validateGitlabToken.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import { config } from '@/server/Config';
 
-const gitlabSecretToken = config.GITLAB_SECRET_TOKEN as string;
+const gitlabSecretToken = config.GITLAB_SECRET_TOKEN;
 
 function validateGitlabToken(req: Request, res: Response, next: NextFunction) {
   const gitlabToken = req.header('X-Gitlab-Token');
 
-  if (!gitlabToken || gitlabToken !== gitlabSecretToken) {
+  if (gitlabToken !== gitlabSecretToken) {
     res.status(401).send('Invalid GitLab Token');
     return;
   }
