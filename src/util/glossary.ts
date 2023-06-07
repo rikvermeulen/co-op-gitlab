5d0d43e013907a069d87411d016ba331ac2fca06
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2023-06-07 14:40:32 +0200

    fix: small fix on prompt

--- src/util/glossary.ts
+++ src/util/glossary.ts
@@ -1,8 +1,9 @@
 export default {
   //openai
-  systemPrompt: 'You are an AI code reviewer that provides feedback in the given code snippet.',
+  systemPrompt:
+    'You are an developer that reviews code that provides feedback top the given code snippet.',
   userPrompt:
-    'Please evaluate the provided code changes below, Focus on substantial updates indicated by "+". Consider improvements in terms of readability, efficiency, best practices, or any possible errors and provide useful feedback. If the changes are trivial, negligible, or don"t meaningfully impact the quality of the code (such as minor string changes, simple variable renaming, missing explanations or comments etc.), you can skip providing feedback. \n If you have suggestions for improvement, provide the updated code inside a markdown collapsible section with the title "Click here to view the revised snippet".\n Language: {language}\n Framework: {framework}\n Code snippet:\n\n{changes}\n\n',
+    'Please give feedback on the provided code changes below, Focus on substantial updates indicated by "+". Consider improvements in terms of readability, efficiency, best practices, or any possible errors and provide useful feedback. If the changes are trivial, negligible, or don"t meaningfully impact the quality of the code (such as minor string changes, simple variable renaming, missing explanations or comments etc.), you can skip providing feedback. \n If you have suggestions for improvement, provide the updated code inside a markdown collapsible section with the title "Click here to view the revised snippet".\n Language: {language}\n Framework: {framework}\n Code snippet:\n\n{changes}\n\n',
 
   sentiment_error:
     'Sorry, but I am unable to provide useful feedback, because the sentiment analyse detected bad words.',
